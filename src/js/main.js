f5bd9fc480f5f53e1f8ab6dba5fa8164c16f7b49
Francisco Villar Salvador <franvlive@gmail.com>
2020-04-17 17:41:19 +0200

    Pait first card with pokemon

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,56 @@
 // Punto de entrada de SCSS
 
 import '../scss/main.scss';
+
+// Declaración de las variables necesarias
+
+let pokedex = document.createElement('section');
+
+let mainContainer = document.querySelector('.main-container');
+
+mainContainer.appendChild(pokedex);
+
+pokedex.classList.add('pokedex');
+
+let card = document.querySelector('.card');
+
+let tagContainer = card.querySelector('.card__types');
+
+let tag = card.querySelector('.card__tag');
+
+// Función que pinta la card del Pokemon
+
+const paintPokemon = () => {
+    let clone = card.cloneNode(true);
+    pokedex.appendChild(clone);
+    clone.classList.remove('card--hidden');
+}
+
+// Conectamos con la API y mostramos los datos
+
+const connectApi = () => {
+    let url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
+
+    fetch(url)
+    .then(response => {
+        response.json()
+        .then(data => {
+            
+            card.querySelector('#cardAsset').setAttribute('src', data.sprites.front_default);
+            card.querySelector('#cardId').innerHTML = data.id;
+            card.querySelector('#cardName').innerHTML = data.name;
+
+            data.types.forEach(type =>{
+                let clone = tag.cloneNode(true);
+                clone.innerHTML = type.type.name;
+                tagContainer.appendChild(clone);
+                clone.classList.remove('card__tag--hidden');
+            });
+
+            paintPokemon();
+        })
+    })
+
+}
+
+connectApi();
