1b61812e6fdba1ad2350ed0da3bb6e5d15ef292b
Alfonso <alfonsosdc@gmail.com>
2020-04-17 19:31:20 +0200

    cambios I

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,50 @@
-export function main(){
-    // code goes here
-    console.log('Hello world')
+const template = document.querySelector('.card')
+
+function addCard(data){
+    const card = template.cloneNode(true)
+    document.querySelector('.container').appendChild(card)
+    card.classList.remove('hidden')
+
+    // rellenar
+    let nameNode = card.querySelector('.name-pokemon')
+    nameNode.innerHTML = data.name
+
+    let imgNode = card.querySelector('.card-img')
+    imgNode.src = data.sprites.front_default
+
+    console.log('id', data.id)
+    let idNode = card.querySelector('.card-id')
+    idNode.innerHTML = data.id
+
+    let typeNode = card.querySelector('.type-pokemon')
+    let typeContainer = card.querySelector('.types-pokemon')
+    data.types.forEach(type => {
+        let clone = typeNode.cloneNode(true)
+        clone.innerHTML = type.type.name
+        typeContainer.appendChild(clone)
+    })
+    typeContainer.children[0].remove()
+
+    return card
 }
+
+export function main(){
+    fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur/')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            addCard(data)
+        })
+}
+
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=3')        
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                //for (let name of names) {
+                    //addCard(data)
+                //}
+            })
+        
+        
+
