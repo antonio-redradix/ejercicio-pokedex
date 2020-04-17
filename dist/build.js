7133527e69cd869a479db55900e676ec45b2d6b0
Cristina <criswalvarez@gmail.com>
2020-04-17 20:52:54 +0200

    adding funcionalities for add cards

--- dist/build.js
+++ dist/build.js
@@ -115,9 +115,49 @@ Object(_main__WEBPACK_IMPORTED_MODULE_0__["main"])();
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "main", function() { return main; });
+const template = document.querySelector('.pokedex_card');
+let cardData = []; // pinta todos los pokemons de cardData
+
+function renderPokemons() {
+  cardData.forEach(data => {
+    addCard(data);
+  });
+}
+
+function addCard(data) {
+  const card = template.cloneNode(true);
+  document.querySelector('.pokedex_cards-container').appendChild(card);
+  card.classList.remove('none');
+  let nameNode = card.querySelector('.pokedex_card-dates-title');
+  nameNode.innerHTML = data.name;
+  let imgNode = card.querySelector('.pokedex_card-img');
+  imgNode.src = data.sprites.front_default;
+  let idNode = card.querySelector('.pokedex_card-img-counter-span');
+  idNode.innerHTML = data.id;
+  let typeNode = card.querySelector('.pokedex_card-pill');
+  let typeContainer = card.querySelector('.pokedex_card-pills-container');
+  data.types.forEach(type => {
+    let clone = typeNode.cloneNode(true);
+    clone.innerHTML = type.type.name;
+    typeContainer.appendChild(clone);
+  });
+  typeContainer.children[0].remove();
+  return card;
+}
+
 function main() {
-  // code goes here
-  console.log('Hello world');
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=10').then(response => response.json()).then(data => {
+    for (let pokemon of data.results) {
+      fetch(pokemon.url).then(response => response.json()).then(pokemonData => {
+        cardData.push(pokemonData);
+
+        if (cardData.length === data.results.length) {
+          // ordenarlo antes de renderizar los pokemon
+          renderPokemons();
+        }
+      });
+    }
+  });
 }
 
 /***/ }),
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,58 @@
-export function main(){
-    // code goes here
-    console.log('Hello world')
+const template = document.querySelector('.pokedex_card')
+
+let cardData = []
+
+// pinta todos los pokemons de cardData
+function renderPokemons(){
+    cardData.forEach(data => {
+        addCard(data)
+    })
 }
+
+function addCard(data){
+    const card = template.cloneNode(true)
+    document.querySelector('.pokedex_cards-container').appendChild(card)
+    card.classList.remove('none')
+
+    let nameNode = card.querySelector('.pokedex_card-dates-title')
+    nameNode.innerHTML = data.name
+
+    let imgNode = card.querySelector('.pokedex_card-img')
+    imgNode.src = data.sprites.front_default
+
+    let idNode = card.querySelector('.pokedex_card-img-counter-span')
+    idNode.innerHTML = data.id
+
+    let typeNode = card.querySelector('.pokedex_card-pill')
+    let typeContainer = card.querySelector('.pokedex_card-pills-container')
+    data.types.forEach(type => {
+        let clone = typeNode.cloneNode(true)
+        clone.innerHTML = type.type.name
+        typeContainer.appendChild(clone)
+    })
+    typeContainer.children[0].remove()
+
+    return card
+}
+
+export function main(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+        .then(response => response.json())
+        .then(data => {
+            for(let pokemon of data.results){
+                fetch(pokemon.url)
+                    .then(response => response.json())
+                    .then(pokemonData => {
+                        cardData.push(pokemonData)
+                        if(cardData.length === data.results.length){
+                            // ordenarlo antes de renderizar los pokemon
+                            
+                            renderPokemons()
+                        }
+                    })
+            }
+        })
+
+}
+
+
